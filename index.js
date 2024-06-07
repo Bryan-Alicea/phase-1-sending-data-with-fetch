6a0b9e52397b75ea17b942b560393c1c37132d9e
Bryan <aliceabryan1996@gmail.com>
2024-06-07 19:48:05 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+function git(){
+    submitData()
+}
+
+document.addEventListener("DOMContentLoaded", git)
+
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        const objectId = data.id 
+
+        const bodyTag = document.querySelector('body')
+        const newElement = document.createElement('div')
+        newElement.innerHTML = objectId
+        bodyTag.appendChild(newElement)
+    })
+    .catch(function(error){
+        const errorMessage = error.message
+
+        const bodyTag = document.querySelector('body')
+        const newElement = document.createElement('div')
+        newElement.innerHTML = errorMessage
+        bodyTag.appendChild(newElement)
+    })
+}
